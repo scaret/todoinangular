662abd1f1dcf0af0c3cd63e58aadc52348cf45c3
scaret <lyc945@gmail.com>
2013-11-12 22:31:45 +0800

    what jeliza give me

---
+++ todo.js
@@ -0,0 +1,27 @@
+function TodoCtrl($scope) {
+  $scope.todos = [
+    {text:'learn angular', done:true, date:2013/12/02},
+    {text:'build an angular app', done:false, date:2013/12/31}];
+ 
+  $scope.addTodo = function() {
+    $scope.todos.push({text:$scope.todoText, done:false, date:$scope.todoDeadline});
+    $scope.todoText = '';
+    $scope.tododeadline = $scope.deadline;
+  };
+ 
+  $scope.remaining = function() {
+    var count = 0;
+    angular.forEach($scope.todos, function(todo) {
+      count += todo.done ? 0 : 1;
+    });
+    return count;
+  };
+ 
+  $scope.archive = function() {
+    var oldTodos = $scope.todos;
+    $scope.todos = [];
+    angular.forEach(oldTodos, function(todo) {
+      if (!todo.done) $scope.todos.push(todo);
+    });
+  };
+}
