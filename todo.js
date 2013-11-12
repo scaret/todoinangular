7382e9db0361ccbbd15b8ac54a6f513096c07d13
scaret <lyc945@gmail.com>
2013-11-12 23:46:40 +0800

    fixed by scaret

--- todo.js
+++ todo.js
@@ -1,14 +1,19 @@
 function TodoCtrl($scope) {
   $scope.todos = [
-    {text:'learn angular', done:true, date:2013/12/02},
-    {text:'build an angular app', done:false, date:2013/12/31}];
+    {text:'learn angular', done:true, date:"2013-12-02"},
+    {text:'build an angular app', done:false, date:"2013-12-31"}];
  
+  var now = new Date();
+  $scope.now = now.getFullYear()+"-" + (now.getMonth()+1) + "-" + now.getDate();
+  $scope.todoDeadline = $scope.now;
+  
   $scope.addTodo = function() {
+	console.log("triggered");
     $scope.todos.push({text:$scope.todoText, done:false, date:$scope.todoDeadline});
     $scope.todoText = '';
-    $scope.tododeadline = $scope.deadline;
+    $scope.todoDeadline = $scope.now;
   };
- 
+
   $scope.remaining = function() {
     var count = 0;
     angular.forEach($scope.todos, function(todo) {
@@ -16,7 +21,7 @@ function TodoCtrl($scope) {
     });
     return count;
   };
- 
+
   $scope.archive = function() {
     var oldTodos = $scope.todos;
     $scope.todos = [];
@@ -24,4 +29,9 @@ function TodoCtrl($scope) {
       if (!todo.done) $scope.todos.push(todo);
     });
   };
+
+  $scope.sort = function(){
+	$scope.todos.sort(function(todo){return  (0 + todo.date.split("-").join("0")) * (todo.done?1:-1);});
+  };
+
 }
